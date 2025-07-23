5387ffbe64b842b986e8031058cdf0ce48c0f096
hayattgd <hayattgd@gmail.com>
2025-07-23 23:14:15 +0900

    refactor: Change structure to make it more extensible

--- src/Editor/UI/MenuBar.cs
+++ src/Editor/UI/MenuBar.cs
@@ -4,60 +4,103 @@ namespace BlinkLab.Editor.UI;
 
 public class MenuBar : IUIElements
 {
+	public struct Menu
+	{
+		public string name;
+		public EditorAction[] actions;
+	}
+
+	public struct EditorAction
+	{
+		public string name;
+		public bool ctrlKey;
+		public bool shiftKey;
+		public bool altKey;
+		public ImGuiKey key;
+		public Action function;
+	}
+
+	readonly Menu[] menus = [
+		new()
+		{
+			name = "File",
+			actions = [
+				new()
+				{
+					name = "New",
+					ctrlKey = true,
+					key = ImGuiKey.N,
+					function = () => {
+						Application.uiManager.AddUI(new NewProjectDialog());
+					}
+				},
+				new()
+				{
+					name = "Load",
+					ctrlKey = true,
+					key = ImGuiKey.O,
+					function = () => {
+						var dialog = new FileSelectDialog("Choose a project");
+						dialog.canOpen = (path) =>
+						{
+							return File.Exists(Path.Combine(path, "UserScript.csproj"));
+						};
+
+						dialog.AfterPrompt += (path, canceled) =>
+						{
+							if (canceled) { return; }
+							Application.LoadProject(path);
+						};
+
+						Application.uiManager.AddUI(dialog);
+					}
+				}
+			]
+		}
+	];
+
 	public void Draw()
 	{
-		if (ImGui.BeginMainMenuBar())
+		ImGui.BeginMainMenuBar();
+
+		foreach (var menu in menus)
 		{
-			if (ImGui.BeginMenu("File"))
+			if (ImGui.BeginMenu(menu.name))
 			{
-				if (ImGui.MenuItem("New", "Ctrl+N"))
+				foreach (var item in menu.actions)
 				{
-					New();
-				}
-				if (ImGui.MenuItem("Load", "Ctrl+O"))
-				{
-					Load();
-				}
-				if (ImGui.MenuItem("Quit"))
-				{
-					Application.Quit();
+					string shortcutKey = "";
+					if (item.ctrlKey) { shortcutKey += "Ctrl + "; }
+					if (item.shiftKey) { shortcutKey += "Shfit + "; }
+					if (item.altKey) { shortcutKey += "Alt + "; }
+					shortcutKey += item.key.ToString();
+
+					if (ImGui.MenuItem(item.name, shortcutKey))
+					{
+						item.function();
+					}
 				}
 				ImGui.EndMenu();
 			}
-			ImGui.EndMainMenuBar();
 		}
 
-		if (ImGui.IsKeyDown(ImGuiKey.ModCtrl))
+		ImGui.EndMainMenuBar();
+
+		foreach (var menu in menus)
 		{
-			if (ImGui.IsKeyPressed(ImGuiKey.N))
+			foreach (var item in menu.actions)
 			{
-				New();
-			}
-			if (ImGui.IsKeyPressed(ImGuiKey.O))
-			{
-				Load();
+				if (
+						item.key != ImGuiKey.None &&
+						(!item.ctrlKey || ImGui.IsKeyDown(ImGuiKey.ModCtrl)) &&
+						(!item.shiftKey || ImGui.IsKeyDown(ImGuiKey.ModShift)) &&
+						(!item.altKey || ImGui.IsKeyDown(ImGuiKey.ModAlt)) &&
+						ImGui.IsKeyPressed(item.key)
+					)
+				{
+					item.function();
+				}
 			}
 		}
-	}
-
-	public static void New()
-	{
-		Application.uiManager.AddUI(new NewProjectDialog());
-	}
-	public static void Load()
-	{
-		var dialog = new FileSelectDialog("Choose a project");
-		dialog.canOpen = (path) =>
-		{
-			return File.Exists(Path.Combine(path, "UserScript.csproj"));
-		};
-
-		dialog.AfterPrompt += (path, canceled) =>
-		{
-			if (canceled) { return; }
-			Application.LoadProject(path);
-		};
-
-		Application.uiManager.AddUI(dialog);
 	}
 }
