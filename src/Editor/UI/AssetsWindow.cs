9c178fab19bada8a0699620eb0d5e62230097e44
hayattgd <hayattgd@gmail.com>
2025-08-20 21:16:45 +0900

    feat: Add Icons to AssetsWindow

--- src/Editor/UI/AssetsWindow.cs
+++ src/Editor/UI/AssetsWindow.cs
@@ -1,16 +1,34 @@
+using BlinkLab.Engine.Rendering;
 using ImGuiNET;
 
 namespace BlinkLab.Editor.UI;
 
 public class AssetsWindow : EditorWindow
 {
+	private static Texture? FileIcon;
+	private static Texture? FolderIcon;
+	private static Texture? ScriptIcon;
+
 	public AssetsWindow()
 	{
 		Title = $"Assets";
 	}
 
+	public static Texture GetTextureFromExtension(string ext) => ext switch
+	{
+		"cs" => ScriptIcon ?? throw new Exception("Icon isn't loaded yet"),
+		_ => FileIcon ?? throw new Exception("Icon isn't loaded yet")
+	};
+
 	public override void Draw()
 	{
+		if (FileIcon == null)
+		{
+			FileIcon = Texture.LoadTexture(Path.Combine(Application.ResourcePath, "Icons", "File.png"));
+			FolderIcon = Texture.LoadTexture(Path.Combine(Application.ResourcePath, "Icons", "Folder.png"));
+			ScriptIcon = Texture.LoadTexture(Path.Combine(Application.ResourcePath, "Icons", "Script.png"));
+		}
+
 		var open = true;
 
 		ImGui.Begin(Title, ref open);
@@ -18,7 +36,8 @@ public class AssetsWindow : EditorWindow
 		for (int i = 0; i < 200; i++)
 		{
 			var cursor = ImGui.GetCursorPos();
-			ImGui.Image(ImGui.GetIO().Fonts.TexID, new(75, 62));
+			ImGui.SetCursorPos(cursor + new System.Numerics.Vector2(5, 0));
+			ImGui.Image(ScriptIcon?.Handle ?? throw new Exception("Icon isn't loaded yet"), new(64, 64));
 			ImGui.SetCursorPos(cursor + new System.Numerics.Vector2(0, 55));
 			ImGui.Text($"{i}");
 			ImGui.SetCursorPos(cursor);
